da88f8e48a345723c24ec1248e3f158998eddb11
mgkjosue <masderxd@gmail.com>
2023-07-04 12:59:33 -0500

    Aplicando CORS

--- Backend/main.py
+++ Backend/main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from routers.consulta import router as consulta_router
 from routers.historia_clinica import router as historia_router
 from routers.paciente import router as paciente_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+
+# Configura el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes de todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos
+    allow_headers=["*"],  # Permite todas las cabeceras
+)
+
 
 app.include_router(consulta_router)
 app.include_router(historia_router)
