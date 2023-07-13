429a5b78f4ad60a8ee693c48f43b62e53ec0d9fc
mgkjosue <josue.alvarez9@hotmail.com>
2023-07-13 08:54:51 -0500

    Comentando el codigo

--- Backend/routers/consulta.py
+++ Backend/routers/consulta.py
@@ -76,6 +76,7 @@ def consulta_to_xlsx(consulta: schemas.Consulta, paciente: schemas.Paciente):
     ws = wb.active
 
     # Suponemos que conoces las celdas exactas donde quieres insertar los datos
+    #Esto es con respecto a los datos de cabecera de la historia clinica
     ws['D2'] = paciente.Nombre
     ws['H2'] = paciente.Apellido
     ws['M2'] = paciente.Sexo
@@ -83,30 +84,53 @@ def consulta_to_xlsx(consulta: schemas.Consulta, paciente: schemas.Paciente):
 
     ws['X1'] = consulta.ID_Consulta
     ws['O2'] = consulta.ID_HistoriaC
+    
+    #Esto es el campo ciclo de vida del paciente 
     ws['B4'] = consulta.RangoAños
 
-    ws['B74'] = consulta.FechaConsulta
+    #Esto ya es con respecto al motico de la consulta
     ws['A7'] = consulta.MotivoC
-    ws['B13'] = consulta.OpcionesAntecedentes
+
+    #Esto es con respecto al punto 2 enfermedad o problema actual
     ws['A10'] = consulta.EnfActual
+
+    #Esto es con respecto al punto 3 antecedentes personales y familiares
+    ws['B13'] = consulta.OpcionesAntecedentes
     ws['A14'] = consulta.Antecedentes
+
+    #Esto es con respecto al punto 4 Signos vitales
     ws['B17'] = consulta.SignosVitales
     ws['F17'] = consulta.FrecuenciaCar
     ws['J17'] = consulta.Temperatura
     ws['O17'] = consulta.FrecuenciaRes
+
+    #Esto es con respecto al punto 5 Examen del sistema estognatico
     ws['C20'] = consulta.OpcionesEstomatognatico
     ws['A21'] = consulta.ExamenEstomat
+
+    #Esto es con respecto al punto 6 Odontograma
     ws['A24'] = consulta.Odontograma
+
+    #Esto es con respecto al punto 7 indicadores de salud bucal
     ws['J48'] = consulta.IndicadoresSalud
     ws['J49'] = consulta.EnfermedadPerio
     ws['J50'] = consulta.MalOclusion
     ws['J51'] = consulta.Fluorosis
+
+    #Esto es con respecto al punto 8 Indices CPO-ceo
     ws['S48'] = consulta.IndicesCPO
+
+    #Esto es con respecto al punto 9 planes de diagnostico, terapeutico y educacional
     ws['D61'] = consulta.OpcionPlan
     ws['A62'] = consulta.PlanDiagnostico
+
+    #Esto es con respecto al punto 11 diagnostico
     ws['A65'] = consulta.Diagnostico
+
+    #Esto es con respecto al punto 12 tratamiento
     ws['D73'] = consulta.Tratamientos
     ws['I73'] = consulta.ProcedimientosE
+    ws['B74'] = consulta.FechaConsulta
     ws['O73'] = consulta.Prescripcion
     ws['V73'] = consulta.Codigo
 
