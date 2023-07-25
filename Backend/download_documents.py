4a3cb277ec0e719dd3588c72efc866d2a1ff66e0
Angel Velez <aivelez1@espe.edu.ec>
2023-07-25 17:27:00 -0500

    NUEVO TEMPLATE

--- Backend/download_documents.py
+++ Backend/download_documents.py
@@ -21,13 +21,13 @@ def consulta_to_xlsx(consulta: schemas.Consulta, paciente: schemas.Paciente):
 
     # Suponemos que conoces las celdas exactas donde quieres insertar los datos
     #Esto es con respecto a los datos de cabecera de la historia clinica
-    ws['D2'] = paciente.Nombre
-    ws['H2'] = paciente.Apellido
-    ws['M2'] = paciente.Sexo
-    ws['N2'] = calcular_edad(paciente.FechaNacimiento)
+    ws['B2'] = paciente.Nombre
+    ws['F2'] = paciente.Apellido
+    ws['J2'] = paciente.Sexo
+    ws['K2'] = calcular_edad(paciente.FechaNacimiento)
 
     #ws['X1'] = consulta.ID_Consulta
-    ws['O2'] = consulta.ID_HistoriaC
+    ws['L2'] = consulta.ID_HistoriaC
     
     #Esto es el campo ciclo de vida del paciente 
     ws['B4'] = consulta.RangoAños
@@ -43,13 +43,13 @@ def consulta_to_xlsx(consulta: schemas.Consulta, paciente: schemas.Paciente):
     ws['A14'] = consulta.Antecedentes
 
     #Esto es con respecto al punto 4 Signos vitales
-    ws['B17'] = consulta.SignosVitales
+    ws['C17'] = consulta.SignosVitales
     ws['F17'] = consulta.FrecuenciaCar
     ws['J17'] = consulta.Temperatura
-    ws['O17'] = consulta.FrecuenciaRes
+    ws['L17'] = consulta.FrecuenciaRes
 
     #Esto es con respecto al punto 5 Examen del sistema estognatico
-    ws['C20'] = consulta.OpcionesEstomatognatico
+    ws['D20'] = consulta.OpcionesEstomatognatico
     ws['A21'] = consulta.ExamenEstomat
 
     #Esto es con respecto al punto 6 Odontograma
@@ -59,82 +59,82 @@ def consulta_to_xlsx(consulta: schemas.Consulta, paciente: schemas.Paciente):
 
     #HIGIENE ORAL SIMPLIFICADA 
     #PIEZAS DENTALES
-    ws['C50'] = consulta.PiezaDental16_17_55
-    ws['C51'] = consulta.PiezaDental11_21_51
-    ws['C52'] = consulta.PiezaDental26_27_65
-    ws['C54'] = consulta.PiezaDental36_37_75
-    ws['C55'] = consulta.PiezaDental31_41_71
-    ws['C56'] = consulta.PiezaDental46_47_85
+    ws['B35'] = consulta.PiezaDental16_17_55
+    ws['B36'] = consulta.PiezaDental11_21_51
+    ws['B37'] = consulta.PiezaDental26_27_65
+    ws['B39'] = consulta.PiezaDental36_37_75
+    ws['B40'] = consulta.PiezaDental31_41_71
+    ws['B41'] = consulta.PiezaDental46_47_85
 
 
     #PLACA 
-    ws['D50'] = consulta.Placa16_17_55
-    ws['D51'] = consulta.Placa11_21_51
-    ws['D52'] = consulta.Placa26_27_65
-    ws['D54'] = consulta.Placa36_37_75
-    ws['D55'] = consulta.Placa31_41_71
-    ws['D56'] = consulta.Placa46_47_85
-    ws['D57'] = consulta.TotalPlaca
+    ws['C35'] = consulta.Placa16_17_55
+    ws['C36'] = consulta.Placa11_21_51
+    ws['C37'] = consulta.Placa26_27_65
+    ws['C39'] = consulta.Placa36_37_75
+    ws['C40'] = consulta.Placa31_41_71
+    ws['C41'] = consulta.Placa46_47_85
+    ws['C42'] = consulta.TotalPlaca
 
     #CALCULO
-    ws['F50'] = consulta.Calculo16_17_55
-    ws['F51'] = consulta.Calculo11_21_51
-    ws['F52'] = consulta.Calculo26_27_65
-    ws['F54'] = consulta.Calculo36_37_75
-    ws['F55'] = consulta.Calculo31_41_71
-    ws['F56'] = consulta.Calculo46_47_85
-    ws['F57'] = consulta.TotalCalculo
+    ws['D35'] = consulta.Calculo16_17_55
+    ws['D36'] = consulta.Calculo11_21_51
+    ws['D37'] = consulta.Calculo26_27_65
+    ws['D39'] = consulta.Calculo36_37_75
+    ws['D40'] = consulta.Calculo31_41_71
+    ws['D41'] = consulta.Calculo46_47_85
+    ws['D42'] = consulta.TotalCalculo
 
 
     #GINGIVITIS
-    ws['H50'] = consulta.Gingivitis16_17_55
-    ws['H51'] = consulta.Gingivitis11_21_51
-    ws['H52'] = consulta.Gingivitis26_27_65
-    ws['H54'] = consulta.Gingivitis36_37_75
-    ws['H55'] = consulta.Gingivitis31_41_71
-    ws['H56'] = consulta.Gingivitis46_47_85
-    ws['H57'] = consulta.TotalGingivitis
+    ws['E35'] = consulta.Gingivitis16_17_55
+    ws['E36'] = consulta.Gingivitis11_21_51
+    ws['E37'] = consulta.Gingivitis26_27_65
+    ws['E39'] = consulta.Gingivitis36_37_75
+    ws['E40'] = consulta.Gingivitis31_41_71
+    ws['E41'] = consulta.Gingivitis46_47_85
+    ws['E42'] = consulta.TotalGingivitis
 
     #ENF. PERIODONTAL
-    ws['M49'] = consulta.EnfermedadPerio
+    ws['H34'] = consulta.EnfermedadPerio
 
     #MAL OCLUSION
-    ws['O49'] = consulta.MalOclusion
+    ws['J34'] = consulta.MalOclusion
 
     #FLUOROSIS
-    ws['Q49'] = consulta.Fluorosis
+    ws['L34'] = consulta.Fluorosis
 
 
 
     #Esto es con respecto al punto 8 Indices CPO-ceo
     #Indice D
-    ws['T49'] = consulta.IndiceC
-    ws['U49'] = consulta.IndiceP
-    ws['V49'] = consulta.IndiceO
-    ws['W49'] = consulta.TotalCPO
+    ws['O34'] = consulta.IndiceC
+    ws['P34'] = consulta.IndiceP
+    ws['Q34'] = consulta.IndiceO
+    ws['R34'] = consulta.TotalCPO
 
     #Indice d
-    ws['T51'] = consulta.Indicedc
-    ws['U51'] = consulta.Indicede
-    ws['V51'] = consulta.Indicedo
-    ws['W51'] = consulta.Totalceo
+    ws['O36'] = consulta.Indicedc
+    ws['P36'] = consulta.Indicede
+    ws['Q36'] = consulta.Indicedo
+    ws['R36'] = consulta.Totalceo
 
     #Esto es con respecto al punto 10 planes de diagnostico, terapeutico y educacional
-    ws['D60'] = consulta.OpcionPlan
-    ws['A61'] = consulta.PlanDiagnostico
+    ws['D45'] = consulta.OpcionPlan
+    ws['A46'] = consulta.PlanDiagnostico
 
     #Esto es con respecto al punto 11 diagnostico
-    ws['B64'] = consulta.Diagnostico
-    ws['M64'] = consulta.Cie
-    ws['P64'] = consulta.PreoDef
-    ws['F66'] = consulta.FechaProximaConsulta
+    ws['B49'] = consulta.Diagnostico
+    ws['L49'] = consulta.Cie
+    ws['Q49'] = consulta.PreoDef
+    ws['E51'] = consulta.FechaProximaConsulta
 
     #Esto es con respecto al punto 12 tratamiento
-    ws['D70'] = consulta.Tratamientos
-    ws['I70'] = consulta.Procedimientos
-    ws['B71'] = consulta.FechaConsulta
-    ws['O70'] = consulta.Prescripcion
-    ws['V70'] = consulta.Codigo
+    ws['D55'] = consulta.Tratamientos
+    ws['D57'] = consulta.Procedimientos
+    ws['B56'] = consulta.FechaConsulta
+    ws['D59'] = consulta.Prescripcion
+    ws['B58'] = consulta.Codigo
 
     # Guardar el archivo temporalmente y retornar su nombre
     filename = f"{paciente.Nombre}{paciente.Apellido}_{paciente.Cedula}_{consulta.FechaConsulta}_consulta.xlsx"
